b15303a9f5246bd3bc6d23084270c7ebd23c924c
Nanda M <f20240739@goa.bits-pilani.ac.in>
2025-06-22 21:47:52 +0530

    First commit
    Created Position with attributes

---
+++ kratos_rover.py
@@ -0,0 +1,18 @@
+
+
+class Position:
+    """
+    
+    Position specifies the x and y coordinates of a certain position,
+and should also hold information about whether the particular coordinate is
+traversable or not.
+
+    Attributes:
+        x: Row index
+        y: Column index
+        traversable: 1 = traversable, 0 = blocked
+    """
+    def __init__(self, x, y, traversable=1):
+        self.x = x
+        self.y = y
+        self.traversable = traversable
